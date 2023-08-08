dad049e78643d2c2856d78d61467101c19dd5016
Mateus Romani <mateusromani@gmail.com>
2023-08-08 18:20:49 -0300

    creating DTO for updating a task

---
+++ U_DO/Data/DTO/UpdateTaskDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace U_DO.Data.DTO
+{
+    public class CreateTaskDto
+    {
+        [Required(ErrorMessage = "The title of the task is mandatory!")]
+        [StringLength(3, ErrorMessage = "The title must be at least 3 characters long!")]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "The status of the task is mandatory!")]
+        public Boolean IsComplete { get; set; }
+
+        [Required(ErrorMessage = "The due date of the task is mandatory!")]
+        public DateTime DueDate { get; set; }
+    }
+}
