a124626a48c4fd0b27f33c57ab4e927236a4f16d
Mateus Romani <mateusromani@gmail.com>
2023-08-09 18:54:41 -0300

    Creating ReadTaskDTO

---
+++ U_DO/Data/DTO/ReadTaskDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace U_DO.Data.DTO;
+
+public class ReadTaskDto
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public Boolean IsComplete { get; set; }
+    public DateTime DueDate { get; set; }
+}
