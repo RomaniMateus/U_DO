5086e6189110f0f3f9e3721fac07892cc9068978
Mateus Romani <mateusromani@gmail.com>
2023-08-09 20:14:34 -0300

    Creating DTO's for the class Course

---
+++ U_DO/Data/DTO/UpdateCourseDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace U_DO.Data.DTO;
+
+public class UpdateTaskDto
+{
+    [Required(ErrorMessage = "The title of the task is mandatory!")]
+    [StringLength(50, ErrorMessage = "The title must be at least 3 characters long!")]
+    public string Title { get; set; }
+
+}
