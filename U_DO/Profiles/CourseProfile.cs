2d7221aef30018d538b2a6fdd785bd6c857f17ac
Mateus Romani <mateusromani@gmail.com>
2023-08-09 20:54:54 -0300

    Creating profile

---
+++ U_DO/Profiles/CourseProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using U_DO.Data.DTO;
+using U_DO.Models;
+
+namespace U_DO.Profiles;
+
+public class CourseProfile : Profile
+{
+    public CourseProfile()
+    {
+        CreateMap<CreateCourseDto, Course>();
+        CreateMap<UpdateCourseDto, Course>();
+        CreateMap<Course, UpdateCourseDto>();
+        CreateMap<Course, ReadCourseDto>();
+    }
+}
