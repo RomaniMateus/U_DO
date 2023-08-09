932767823a10ea3f7485a48a1f3cbc7dbe5fb9a3
Mateus Romani <mateusromani@gmail.com>
2023-08-09 20:53:22 -0300

    fixing DTO

--- U_DO/Data/DTO/UpdateCourseDto.cs
+++ U_DO/Data/DTO/UpdateCourseDto.cs
@@ -2,10 +2,10 @@
 
 namespace U_DO.Data.DTO;
 
-public class UpdateTaskDto
+public class UpdateCourseDto
 {
-    [Required(ErrorMessage = "The title of the task is mandatory!")]
-    [StringLength(50, ErrorMessage = "The title must be at least 3 characters long!")]
-    public string Title { get; set; }
+    [Required(ErrorMessage = "The name of the course is mandatory!")]
+    [StringLength(50, ErrorMessage = "The name must be at least 3 characters long!")]
+    public string Name { get; set; }
 
 }
